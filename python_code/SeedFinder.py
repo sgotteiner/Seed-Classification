eb265a2f847ec0275c3e9120a203a2b7b12eb65c
sgotteiner <sgotteiner@gmail.com>
2025-04-10 11:35:07 +0300

    seed rename bug fix

--- python_code/SeedFinder.py
+++ python_code/SeedFinder.py
@@ -14,11 +14,11 @@ def find_paths_of_seeds(directory):
 
 def seed_rename(old_name):
     split_old_name = old_name.split(os.sep)
-    tray_number = split_old_name[-3][1]
+    tray_number = split_old_name[-3][1:-1]
     position = split_old_name[-1].split('.')[0]
     row = int(ord(position[1]) - ord('A'))
     column = ord(position[0]) - ord('0') - 1
-    column = column if split_old_name[-3][2] == 'L' else column + 7
+    column = column if split_old_name[-3][-1] == 'L' else column + 7
     file_type = split_old_name[-1].split('.')[1]
     new_name = f'Tray_{tray_number}_row_{row}_column_{column}.{file_type}'
     return new_name
